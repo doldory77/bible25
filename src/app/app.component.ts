40a17296da5ff23f82e47e49ce81b143f91e979e
doldory77 <doldory33@gmail.com>
2018-03-30 10:07:39 +0900

    android, ios platform 추가 및 sidemenu 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { menu } from '../menu/menu';
 
 @Component({
   templateUrl: 'app.html'
@@ -16,7 +17,12 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  /* menu */
+  todayMenuIsOpen = false;
+
+  constructor(public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -36,7 +42,15 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
+  menuData = menu;
+
+  openPage(menu: string) {
+    let menuItem: any = this.menuData[menu];
+    console.log(menuItem);
+    menuItem.selected = !menuItem.selected; 
+  }
+
+  openPageBack(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
