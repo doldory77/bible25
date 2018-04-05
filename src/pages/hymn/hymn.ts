5722717e3e8c524d138c280ebf3be1bd618b58c9
doldory <doldory@Macui-MacBook-Pro.local>
2018-04-06 07:20:49 +0900

    찬송 검색옵션 화면추가

--- src/pages/hymn/hymn.ts
+++ src/pages/hymn/hymn.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { PlayerComponent } from '../../components/player/player';
 import { DbProvider } from '../../providers/db/db';
 
@@ -20,9 +20,18 @@ export class HymnPage {
   topMenuData: {title:string, menuIdx:number, selected:boolean}[] = [];
   hymnData: {p_num:string, p_num_old:string, subject:string}[] = [];
 
+  isNoneVisibleSearch: boolean = true;
+  searchType: string = '0'
+  searchKeyWord: string = '';
+  selectOptions = {
+    title: 'Pizza Toppings',
+  };
+
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    public db: DbProvider) {
+    public db: DbProvider,
+    public alertCtrl: AlertController) {
 
       this.topMenuData.push({title:'전체', menuIdx:0, selected:false});
       this.topMenuData.push({title:'100', menuIdx:1, selected:true});
@@ -63,6 +72,36 @@ export class HymnPage {
       .catch(err => {
         console.log('==========> hymn data load error: ', err);
       })  
+  }
+
+  execSearch() {
+    alert('search do...')
+  }
+
+  showIndex() {
+    this.alertCtrl.create({
+      title: '분류검색',
+      inputs: [
+        {type:'radio', label:'AAA', value:'1'},
+        {type:'radio', label:'AAA', value:'2'},
+        {type:'radio', label:'AAA', value:'3'},
+        {type:'radio', label:'AAA', value:'4'},
+        {type:'radio', label:'AAA', value:'5'},
+        {type:'radio', label:'AAA', value:'6'},
+      ],
+      buttons: [
+        {
+          text:'취소',
+          role:'cancel'
+        },
+        {
+          text:'검색',
+          handler: data => {
+            console.log(data);
+          }
+        }
+      ]
+    }).present();
   }
 
 }
