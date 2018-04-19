7059d3840cb70eb57d8d6922a263398eb095fc31
doldory77 <doldory33@gmail.com>
2018-04-19 09:47:06 +0900

    Pinchable 베이스클래스 생성

--- src/pages/learn-bible-detail/learn-bible-detail.ts
+++ src/pages/learn-bible-detail/learn-bible-detail.ts
@@ -5,30 +5,32 @@ import { PlayerProvider } from '../../providers/player/player';
 import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
 import { ContentDimensions } from 'ionic-angular/components/content/content';
+import { Pinchable } from '../../model/pinchable';
 
-const MAX_SCALE = 2.1;
-const MIN_SCALE = 0.9;
-const BASE_SCALE = 1.5;
+// const MAX_SCALE = 2.1;
+// const MIN_SCALE = 0.9;
+// const BASE_SCALE = 1.5;
 
 @IonicPage()
 @Component({
   selector: 'page-learn-bible-detail',
   templateUrl: 'learn-bible-detail.html',
 })
-export class LearnBibleDetailPage {
+export class LearnBibleDetailPage extends Pinchable {
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     private db: DbProvider,
     private player: PlayerProvider) {
+      super();
       if (navParams.get('book')) this.bibleBook = navParams.get('book');
       if (navParams.get('jang')) this.bibleJang = navParams.get('jang');
   }
 
-  public fontSize = `${BASE_SCALE}rem`;
-  private scale = BASE_SCALE;
-  private alreadyScaled = BASE_SCALE;
-  private isScaling = false;
+  // public fontSize = `${BASE_SCALE}rem`;
+  // private scale = BASE_SCALE;
+  // private alreadyScaled = BASE_SCALE;
+  // private isScaling = false;
 
   @ViewChild(Content) content: Content;
 
@@ -122,59 +124,59 @@ export class LearnBibleDetailPage {
   }
 
   // =================== PINCH ========================
-  public onPinchStart(e) {
+  // public onPinchStart(e) {
 
-    // flag that sets the class to disable scrolling
-    // console.log('onPinchStart')
-    this.isScaling = true;
-  }
+  //   // flag that sets the class to disable scrolling
+  //   // console.log('onPinchStart')
+  //   this.isScaling = true;
+  // }
 
-  // called at (pinchend) and (pinchcancel)
-  public onPinchEnd(e) {
+  // // called at (pinchend) and (pinchcancel)
+  // public onPinchEnd(e) {
 
-    // flip the flag, enable scrolling
-    this.isScaling = false;
+  //   // flip the flag, enable scrolling
+  //   this.isScaling = false;
 
-    // adjust the amount we already scaled
-    this.alreadyScaled = this.scale * this.alreadyScaled;
-    // console.log('onPinchEnd')
-  }
+  //   // adjust the amount we already scaled
+  //   this.alreadyScaled = this.scale * this.alreadyScaled;
+  //   // console.log('onPinchEnd')
+  // }
 
-  public onPinchMove(e) {
+  // public onPinchMove(e) {
 
-    // set the scale so we can track it globally
-    this.scale = e.scale;
+  //   // set the scale so we can track it globally
+  //   this.scale = e.scale;
 
-    // total amount we scaled
-    let totalScaled = this.alreadyScaled * e.scale;
+  //   // total amount we scaled
+  //   let totalScaled = this.alreadyScaled * e.scale;
 
-    // did we hit the max scale (pinch out)
-    if (totalScaled >= MAX_SCALE) {
+  //   // did we hit the max scale (pinch out)
+  //   if (totalScaled >= MAX_SCALE) {
 
-      // fix the scale by calculating it, don't use the e.scale
-      // scenario: an insane quick pinch out will offset the this.scale
-      this.scale = MAX_SCALE / this.alreadyScaled;
-      totalScaled = MAX_SCALE;
+  //     // fix the scale by calculating it, don't use the e.scale
+  //     // scenario: an insane quick pinch out will offset the this.scale
+  //     this.scale = MAX_SCALE / this.alreadyScaled;
+  //     totalScaled = MAX_SCALE;
 
-      // did we hit the min scale (pinch in)
-    } else if (totalScaled <= MIN_SCALE) {
+  //     // did we hit the min scale (pinch in)
+  //   } else if (totalScaled <= MIN_SCALE) {
 
-      // fix the scale
-      this.scale = MIN_SCALE / this.alreadyScaled;
-      totalScaled = MIN_SCALE;
+  //     // fix the scale
+  //     this.scale = MIN_SCALE / this.alreadyScaled;
+  //     totalScaled = MIN_SCALE;
 
-    }
+  //   }
 
-    let fontSize = Math.round(totalScaled * 10) / 10;
+  //   let fontSize = Math.round(totalScaled * 10) / 10;
 
-    // change the fontsize every 3 decimals in scale change
-    if ((fontSize * 10) % 3 === 0) {
+  //   // change the fontsize every 3 decimals in scale change
+  //   if ((fontSize * 10) % 3 === 0) {
 
-      // update the fontsize
-      this.fontSize = `${fontSize}rem`;
-    }
+  //     // update the fontsize
+  //     this.fontSize = `${fontSize}rem`;
+  //   }
 
-  }
+  // }
   // =================== PINCH ========================  
 
 }
